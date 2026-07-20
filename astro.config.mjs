208224cade2fa1b1738a904841565608147aed6f
Seth Hendrikz <166507213+sethsticle@users.noreply.github.com>
2026-07-20 16:39:08 +0200

    Redirect and CF setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
